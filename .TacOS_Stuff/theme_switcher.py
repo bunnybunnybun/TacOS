4d7fbaca1b0a6eb0798c44007927f3faf183522f
Carlisle Poe <carlislepoe@gmail.com>
2025-10-20 21:10:01 -0700

    Rebranded everything to TacOS instead of ScrollingDE.Also began work on the TacOS settings app!

--- .TacOS_Stuff/theme_switcher.py
+++ .TacOS_Stuff/theme_switcher.py
@@ -18,31 +18,31 @@ while True:
         sg.theme('Light Blue')
         window.close()
         window = sg.Window('Theme Switcher', create_layout(), resizable = False)
-        os.system("killall waybar; waybar -c ~/.scrolling_de_stuff/assets/waybar/config.jsonc -s ~/.scrolling_de_stuff/assets/waybar/daisies_style.css & disown")
+        os.system("killall waybar; waybar -c ~/.TacOS_Stuff/assets/waybar/config.jsonc -s ~/.TacOS_Stuff/assets/waybar/daisies_style.css & disown")
         os.system("killall hyprpaper")
         time.sleep(0.1)
-        os.system("hyprpaper --config ~/.scrolling_de_stuff/assets/hyprpaper/daisies_hyprpaper.conf & disown")
-        os.system("cp ~/.scrolling_de_stuff/assets/niri/daisy_config.kdl ~/.config/niri/config.kdl")
+        os.system("hyprpaper --config ~/.TacOS_Stuff/assets/hyprpaper/daisies_hyprpaper.conf & disown")
+        os.system("cp ~/.TacOS_Stuff/assets/niri/daisy_config.kdl ~/.config/niri/config.kdl")
 
     if event == 'Minimal':
         sg.theme('Python')
         window.close()
         window = sg.Window('Theme Switcher', create_layout(), resizable = False)
-        os.system("killall waybar; waybar -c ~/.scrolling_de_stuff/assets/waybar/config.jsonc -s ~/.scrolling_de_stuff/assets/waybar/minimal_style.css & disown")
+        os.system("killall waybar; waybar -c ~/.TacOS_Stuff/assets/waybar/config.jsonc -s ~/.TacOS_Stuff/assets/waybar/minimal_style.css & disown")
         os.system("killall hyprpaper")
         time.sleep(0.1)
-        os.system("hyprpaper --config ~/.scrolling_de_stuff/assets/hyprpaper/minimal_hyprpaper.conf & disown")
-        os.system("cp ~/.scrolling_de_stuff/assets/niri/minimal_config.kdl ~/.config/niri/config.kdl")
+        os.system("hyprpaper --config ~/.TacOS_Stuff/assets/hyprpaper/minimal_hyprpaper.conf & disown")
+        os.system("cp ~/.TacOS_Stuff/assets/niri/minimal_config.kdl ~/.config/niri/config.kdl")
 
     if event == "Magic":
         sg.theme('Dark Blue 1')
         window.close()
         window = sg.Window('Theme Switcher', create_layout(), resizable = False)
-        os.system("killall waybar; waybar -c ~/.scrolling_de_stuff/assets/waybar/config.jsonc -s ~/.scrolling_de_stuff/assets/waybar/magic_style.css & disown")
+        os.system("killall waybar; waybar -c ~/.TacOS_Stuff/assets/waybar/config.jsonc -s ~/.TacOS_Stuff/assets/waybar/magic_style.css & disown")
         os.system("killall hyprpaper")
         time.sleep(0.1)
-        os.system("hyprpaper --config ~/.scrolling_de_stuff/assets/hyprpaper/magic_hyprpaper.conf & disown")
-        os.system("cp ~/.scrolling_de_stuff/assets/niri/magic_config.kdl ~/.config/niri/config.kdl")
+        os.system("hyprpaper --config ~/.TacOS_Stuff/assets/hyprpaper/magic_hyprpaper.conf & disown")
+        os.system("cp ~/.TacOS_Stuff/assets/niri/magic_config.kdl ~/.config/niri/config.kdl")
 
     if event == sg.WIN_CLOSED:
         break
--- install_script.sh
+++ install_script.sh
@@ -2,17 +2,17 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 sudo pacman -Syu
-sudo pacman -S --needed --noconfirm nemo fastfetch base-devel git sddm niri kitty mako fuzzel swaybg swayidle swaylock waybar xdg-desktop-portal-gnome python-pip tk xwayland-satellite hyprpaper ttf-jetbrains-mono-nerd vim starship
+sudo pacman -S --needed --noconfirm nemo fastfetch base-devel git sddm niri kitty mako fuzzel swaybg swayidle swaylock waybar xdg-desktop-portal-gnome python-pip tk xwayland-satellite hyprpaper ttf-jetbrains-mono-nerd vim starship gtk3 gtk4 libadwaita python-gobject
 sudo systemctl enable sddm
 pip install FreeSimpleGUI --break-system-packages
-sudo cp $SCRIPT_DIR/.scrolling_de_stuff ~/ -r
+sudo cp $SCRIPT_DIR/.TacOS_Stuff ~/ -r
 sudo cp $SCRIPT_DIR/Theme\ Switcher.desktop /usr/share/applications/Theme\ Switcher.desktop
-cp ~/.scrolling_de_stuff/assets/niri/ ~/.config/ -r
-cp ~/.scrolling_de_stuff/assets/kitty/ ~/.config/ -r
-cp ~/.scrolling_de_stuff/assets/starship/starship.toml ~/.config/
-sudo cp ~/.scrolling_de_stuff/assets/fastfetch ~/.config/ -r
-sudo cp ~/.scrolling_de_stuff/assets/icons /usr/share/ -r
-sudo cp -r ~/.scrolling_de_stuff/assets/fuzzel /etc/xdg/
+cp ~/.TacOS_Stuff/assets/niri/ ~/.config/ -r
+cp ~/.TacOS_Stuff/assets/kitty/ ~/.config/ -r
+cp ~/.TacOS_Stuff/assets/starship/starship.toml ~/.config/
+sudo cp ~/.TacOS_Stuff/assets/fastfetch ~/.config/ -r
+sudo cp ~/.TacOS_Stuff/assets/icons /usr/share/ -r
+sudo cp -r ~/.TacOS_Stuff/assets/fuzzel /etc/xdg/
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 if [ -f "/home/carlisle/.yay_has_been_installed" ]; then
     echo "Yay has already been installed"
@@ -21,4 +21,4 @@ else
 fi
 yay -S --needed ungoogled-chromium-bin --noconfirm
 
-echo "ScrollingDE setup finished! It is recommend that you reboot now. (You can reboot by typing reboot and then pressing enter)"
+echo "TacOS setup finished! It is recommend that you reboot now. (You can reboot by typing reboot and then pressing enter)"
