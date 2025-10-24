ec0a1f44ced1d4ea95db03d22351c920f895d2a0
Carlisle Poe <carlislepoe@gmail.com>
2025-10-24 11:32:30 -0700

    Added a color menu for choosing the focus ring color!

--- .TacOS_Stuff/TacOS_Settings_App/pages/General_Settings.py
+++ .TacOS_Stuff/TacOS_Settings_App/pages/General_Settings.py
@@ -1,10 +1,14 @@
 import gi
 import os
 import time
-from pages.General_Settings_Subfiles.set_focus_ring_width import okdoitnowdaisies
-from pages.General_Settings_Subfiles.set_focus_ring_width import okdoitnowminimal
-from pages.General_Settings_Subfiles.set_focus_ring_width import okdoitnowmagic
-from pages.General_Settings_Subfiles.set_focus_ring_width import okdoitnowfall
+from pages.General_Settings_Subfiles.set_focus_ring_width import set_focus_ring_width_daisies
+from pages.General_Settings_Subfiles.set_focus_ring_width import set_focus_ring_width_minimal
+from pages.General_Settings_Subfiles.set_focus_ring_width import set_focus_ring_width_magic
+from pages.General_Settings_Subfiles.set_focus_ring_width import set_focus_ring_width_fall
+from pages.General_Settings_Subfiles.set_focus_ring_color import set_focus_ring_color_daisies
+from pages.General_Settings_Subfiles.set_focus_ring_color import set_focus_ring_color_minimal
+from pages.General_Settings_Subfiles.set_focus_ring_color import set_focus_ring_color_magic
+from pages.General_Settings_Subfiles.set_focus_ring_color import set_focus_ring_color_fall
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
@@ -36,6 +40,8 @@ class GeneralSettingsPage(Gtk.Box):
         dark_mode_button.connect("clicked", self.Dark_Mode)
         black_mode_button = Gtk.Button(label="Black mode")
         black_mode_button.connect("clicked", self.Black_Mode)
+        color_chooser_button = Gtk.ColorButton()
+        color_chooser_button.connect("color-set", self.on_color_set)
 
 
         label_2 = Gtk.Label(label="Set focus ring width:")
@@ -55,12 +61,33 @@ class GeneralSettingsPage(Gtk.Box):
         self.add(light_mode_button)
         self.add(dark_mode_button)
         self.add(black_mode_button)
+        self.add(color_chooser_button)
         self.add(label_2)
         self.add(self.focus_ring_width_scale)
         theme_grid.attach(theme_button_1, 1, 0, 1, 1)
         theme_grid.attach_next_to(theme_button_2, theme_button_1, Gtk.PositionType.RIGHT, 1, 1)
         theme_grid.attach_next_to(theme_button_3, theme_button_1, Gtk.PositionType.BOTTOM, 1, 1)
         theme_grid.attach_next_to(theme_button_4, theme_button_3, Gtk.PositionType.RIGHT, 1, 1)
+
+    def on_color_set(self, color_button):
+        rgba = color_button.get_rgba()
+
+        r = int(rgba.red * 255)
+        g = int(rgba.green * 255)
+        b = int(rgba.blue * 255)
+        a = int(rgba.alpha * 255)
+
+        color_value = f"#{r:02x}{g:02x}{b:02x}{a:02x}"
+        print(f"Color = {color_value}")
+
+        if os.path.exists(f"{self.script_dir}/General_Settings_Subfiles/current_theme_is_daisies"):
+            set_focus_ring_color_daisies(None, color_value)
+        elif os.path.exists(f"{self.script_dir}/General_Settings_Subfiles/current_theme_is_minimal"):
+            set_focus_ring_color_minimal(None, color_value)
+        elif os.path.exists(f"{self.script_dir}/General_Settings_Subfiles/current_theme_is_magic"):
+            set_focus_ring_color_magic(None, color_value)
+        elif os.path.exists(f"{self.script_dir}/General_Settings_Subfiles/current_theme_is_fall"):
+            set_focus_ring_color_fall(None, color_value)
 
     def Light_Mode(self, widget):
         os.system('gsettings set org.gnome.desktop.interface gtk-theme "Emacs"')
@@ -74,13 +101,13 @@ class GeneralSettingsPage(Gtk.Box):
     def on_scale_changed(self, scale):
         value = scale.get_value()
         if os.path.exists(f"{self.script_dir}/General_Settings_Subfiles/current_theme_is_daisies"):
-            okdoitnowdaisies(None, value)
+            set_focus_ring_width_daisies(None, value)
         elif os.path.exists(f"{self.script_dir}/General_Settings_Subfiles/current_theme_is_minimal"):
-            okdoitnowminimal(None, value)
+            set_focus_ring_width_minimal(None, value)
         elif os.path.exists(f"{self.script_dir}/General_Settings_Subfiles/current_theme_is_magic"):
-            okdoitnowmagic(None, value)
+            set_focus_ring_width_magic(None, value)
         elif os.path.exists(f"{self.script_dir}/General_Settings_Subfiles/current_theme_is_fall"):
-            okdoitnowfall(None, value)
+            set_focus_ring_width_fall(None, value)
 
     def Set_Theme_Daisies(self, widget):
         os.system("killall waybar; waybar -c ~/.TacOS_Stuff/assets/waybar/config.jsonc -s ~/.TacOS_Stuff/assets/waybar/daisies_style.css & disown")
--- .TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/set_focus_ring_color.py
+++ .TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/set_focus_ring_color.py
@@ -1,7 +1,7 @@
 import kdl
 import os
 
-def update_width_in_kdl(file_path, new_width=10):
+def update_focus_ring_color_in_kdl(file_path, new_color="#0f4c5c"):
     with open(file_path, 'r') as f:
         doc = kdl.parse(f.read())
 
@@ -16,10 +16,10 @@ def update_width_in_kdl(file_path, new_width=10):
 
                 for child in all_children:
                     print(f"   Child: {child.name} with args={child.args}")
-                    if child.name == "width" and child.args:
+                    if child.name == "active-color" and child.args:
                         print(f"Current width: {child.args[0]}")
-                        child.args[0] = float(new_width)
-                        print(f"Updated width: {new_width}")
+                        child.args[0] = (new_color)
+                        print(f"Updated width: {new_color}")
                         break
 
 
@@ -27,30 +27,30 @@ def update_width_in_kdl(file_path, new_width=10):
         f.write(str(doc))
 
 
-def set_focus_ring_width_daisies(widget, scale_value=5):
-    width_value = scale_value if scale_value is not None else 5
-    update_width_in_kdl("/home/carlisle/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/daisies_layout_part_2.kdl", width_value)
+def set_focus_ring_color_daisies(widget, color_value="#0f4c5c"):
+    width_value = color_value if color_value is not None else "#0f4c5c"
+    update_focus_ring_color_in_kdl("/home/carlisle/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/daisies_layout_part_2.kdl", width_value)
     os.system("cat ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/daisies_layout_part_1.kdl ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/daisies_layout_part_2.kdl ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/daisies_layout_part_3.kdl > ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout_final.kdl")
     os.system("cat ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/daisies_the_rest_lol.kdl ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout_final.kdl > ~/.config/niri/config.kdl")
     print("test")
 
-def set_focus_ring_width_minimal(widget, scale_value=5):
-    width_value = scale_value if scale_value is not None else 5
-    update_width_in_kdl("/home/carlisle/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/minimal_layout_part_2.kdl", width_value)
+def set_focus_ring_color_minimal(widget, color_value="#0f4c5c"):
+    width_value = color_value if color_value is not None else "#0f4c5c"
+    update_focus_ring_color_in_kdl("/home/carlisle/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/minimal_layout_part_2.kdl", width_value)
     os.system("cat ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/minimal_layout_part_1.kdl ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/minimal_layout_part_2.kdl ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/minimal_layout_part_3.kdl > ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout_final.kdl")
     os.system("cat ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/minimal_the_rest_lol.kdl ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout_final.kdl > ~/.config/niri/config.kdl")
     print("test")
 
-def set_focus_ring_width_magic(widget, scale_value=5):
-    width_value = scale_value if scale_value is not None else 5
-    update_width_in_kdl("/home/carlisle/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/magic_layout_part_2.kdl", width_value)
+def set_focus_ring_color_magic(widget, color_value="#0f4c5c"):
+    width_value = color_value if color_value is not None else "#0f4c5c"
+    update_focus_ring_color_in_kdl("/home/carlisle/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/magic_layout_part_2.kdl", width_value)
     os.system("cat ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/magic_layout_part_1.kdl ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/magic_layout_part_2.kdl ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/magic_layout_part_3.kdl > ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout_final.kdl")
     os.system("cat ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/magic_the_rest_lol.kdl ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout_final.kdl > ~/.config/niri/config.kdl")
     print("test")
 
-def set_focus_ring_width_fall(widget, scale_value=5):
-    width_value = scale_value if scale_value is not None else 5
-    update_width_in_kdl("/home/carlisle/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/fall_layout_part_2.kdl", width_value)
+def set_focus_ring_color_fall(widget, color_value="#0f4c5c"):
+    width_value = color_value if color_value is not None else "#0f4c5c"
+    update_focus_ring_color_in_kdl("/home/carlisle/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/fall_layout_part_2.kdl", width_value)
     os.system("cat ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/fall_layout_part_1.kdl ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/fall_layout_part_2.kdl ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout/fall_layout_part_3.kdl > ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout_final.kdl")
     os.system("cat ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/fall_the_rest_lol.kdl ~/TacOS/TacOS/.TacOS_Stuff/TacOS_Settings_App/pages/General_Settings_Subfiles/niri_config_file_sections/layout_final.kdl > ~/.config/niri/config.kdl")
     print("test")
