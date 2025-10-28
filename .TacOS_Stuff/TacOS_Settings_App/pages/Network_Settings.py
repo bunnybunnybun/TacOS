401a3a5540c74cc393c638064046330c6a8c4f1e
Carlisle Poe <carlislepoe@gmail.com>
2025-10-28 11:59:55 -0700

    Edited the starship terminal prompt, and made some progress in getting the network settings page in the settings app to be functional.

--- .TacOS_Stuff/TacOS_Settings_App/pages/Network_Settings.py
+++ .TacOS_Stuff/TacOS_Settings_App/pages/Network_Settings.py
@@ -1,5 +1,9 @@
 import gi
+import os
 from .view_networks import view_networks_ig
+
+gi.require_version("NM", "1.0")
+from gi.repository import GLib, NM
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
@@ -13,13 +17,14 @@ class NetworkSettingsPage(Gtk.Box):
 
         view_networks_button = Gtk.Button(label="View available networks")
         view_networks_button.connect("clicked", self.view_networks)
-        ssid_input = Gtk.Entry()
-        ssid_input.set_placeholder_text("SSID")
+        self.ssid_input = Gtk.Entry()
+        self.ssid_input.set_placeholder_text("SSID")
         pass_input = Gtk.Entry()
         pass_input.set_placeholder_text("Password")
         connect_button = Gtk.Button(label="Connect")
+        connect_button.connect("clicked", self.testing)
         self.add(view_networks_button)
-        self.add(ssid_input)
+        self.add(self.ssid_input)
         self.add(pass_input)
         self.add(connect_button)
 
@@ -27,3 +32,18 @@ class NetworkSettingsPage(Gtk.Box):
         view_networks_window = view_networks_ig()
         view_networks_window.show_all()
         print("test")
+
+    def testing(self, widget):
+        client = NM.Client.new(None)
+        print("version ", client.get_version())
+        devices = client.get_devices()
+        print("Devices:")
+        for device in devices:
+            print("    name:", device.get_iface())
+            print("    type:", device.get_type_description())
+            print("    state:", device.get_state().value_nick)
+
+    def connect(self, widget):
+        ssid = self.ssid_input.get_text().strip()
+        print(ssid)
+        os.system(f'nmcli device wifi connect "{ssid}" --ask')
