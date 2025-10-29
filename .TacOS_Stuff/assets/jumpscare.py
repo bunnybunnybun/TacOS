5acb6c1339c519e1961e13fe502360b75ff23284
Carlisle Poe <carlislepoe@gmail.com>
2025-10-28 22:36:47 -0700

    Created a taco jumpscare... now I just need to figure out where to implement it.

---
+++ .TacOS_Stuff/assets/jumpscare.py
@@ -0,0 +1,54 @@
+import gi
+import subprocess
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, Gdk, GLib
+gi.require_version('GtkLayerShell', '0.1')
+from gi.repository import GtkLayerShell
+
+css_provider = Gtk.CssProvider()
+
+css = """
+window {
+    background-color: rgba(0, 0, 0, 0.0);
+}
+"""
+css_provider.load_from_data(css.encode())
+
+settings = Gtk.Settings.get_default()
+settings.set_property("gtk-theme-name", "Adwaita")
+settings.set_property("gtk-application-prefer-dark-theme", True)
+
+screen = Gdk.Screen.get_default()
+style_context = Gtk.StyleContext()
+style_context.add_provider_for_screen(
+    screen,
+    css_provider,
+    Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+)
+
+class MainWindow(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="Jumpscare!!!")
+        self.set_default_size(600, 400)
+
+        GtkLayerShell.init_for_window(self)
+
+        image = Gtk.Image.new_from_file("jumpscare_image.png")
+        image.set_opacity(1)
+        self.add(image)
+
+        self.show_all()
+        self.play_audio()
+
+        GLib.timeout_add_seconds(3, self.destroy)
+
+    def play_audio(self):
+        subprocess.Popen(
+        ["mpv", "taco-crunch.mp3"],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+        )
+win = MainWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
